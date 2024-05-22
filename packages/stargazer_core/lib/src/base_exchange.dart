3bb0c4499785b168e1eb1203fc99edf2b3a869fc
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-22 08:54:45 +0500

    Update files

--- packages/stargazer_core/lib/src/base_exchange.dart
+++ packages/stargazer_core/lib/src/base_exchange.dart
@@ -1,3 +1,59 @@
 import 'exchange.dart';
+import 'exchange_information.dart';
+import 'exchange_metadata.dart';
+import 'instrument/instrument.dart';
+import 'service/account_service.dart';
+import 'service/market_data_service.dart';
+import 'service/trade_service.dart';
 
-abstract base class BaseExchange implements Exchange {}
+abstract class BaseExchange implements Exchange {
+  @override
+  void applySpecification(ExchangeInformation exchangeInformation) {}
+
+  @override
+  AccountService getAccountService() {
+    // TODO: implement getAccountService
+    throw UnimplementedError();
+  }
+
+  @override
+  ExchangeInformation getDefaultExchangeInformation() {
+    // TODO: implement getDefaultExchangeInformation
+    throw UnimplementedError();
+  }
+
+  @override
+  ExchangeInformation getExchangeInformation() {
+    // TODO: implement getExchangeInformation
+    throw UnimplementedError();
+  }
+
+  @override
+  ExchangeMetadata getExchangeMetadata() {
+    // TODO: implement getExchangeMetadata
+    throw UnimplementedError();
+  }
+
+  @override
+  List<Instrument> getInstruments() {
+    // TODO: implement getInstruments
+    throw UnimplementedError();
+  }
+
+  @override
+  MarketDataService getMarketDataService() {
+    // TODO: implement getMarketDataService
+    throw UnimplementedError();
+  }
+
+  @override
+  TradeService getTradeService() {
+    // TODO: implement getTradeService
+    throw UnimplementedError();
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
+}
--- packages/stargazer_core/lib/src/exchange.dart
+++ packages/stargazer_core/lib/src/exchange.dart
@@ -1,17 +1,26 @@
 import 'exchange_information.dart';
 import 'exchange_metadata.dart';
 import 'instrument/instrument.dart';
+import 'service/account_service.dart';
+import 'service/market_data_service.dart';
+import 'service/trade_service.dart';
 
 abstract interface class Exchange {
   ExchangeInformation getExchangeInformation();
+
+  ExchangeInformation getDefaultExchangeInformation();
 
   ExchangeMetadata getExchangeMetadata();
 
   List<Instrument> getInstruments();
 
-  void applySpecification();
+  void applySpecification(ExchangeInformation exchangeInformation);
 
+  MarketDataService getMarketDataService();
 
+  TradeService getTradeService();
 
+  AccountService getAccountService();
+
+  void init();
 }
-
